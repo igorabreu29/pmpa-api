affe241cc075d6a91fc1f765547da8dd1bab0f0b
igorabreu29 <abreusense@gmail.com>
2024-07-23 16:56:23 -0300

    chore: add dependencies

---
+++ vitest.config.e2e.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitest/config'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import swc from 'unplugin-swc'
+
+export default defineConfig({
+  plugins: [tsConfigPaths(), swc.vite({ module: { type: 'es6' } })],
+  test: {
+    environmentMatchGlobs: [['src/infra/http/controllers/**', 'prisma']],
+    include: ['**/*.e2e-spec.ts'],
+  }
+})
