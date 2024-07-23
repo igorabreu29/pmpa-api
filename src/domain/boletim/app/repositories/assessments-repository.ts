f8c79a88eb75d024beb658bca9283d19450248cb
igorabreu29 <abreusense@gmail.com>
2024-07-23 10:19:52 -0300

    refactor: renaming assesment to assessment

--- src/domain/boletim/app/repositories/assessments-repository.ts
+++ src/domain/boletim/app/repositories/assessments-repository.ts
@@ -15,8 +15,8 @@ export abstract class AssessmentsRepository {
   abstract findById({ id }: { id: string }): Promise<Assessment | null>
   abstract findByStudentIdAndCourseId({ studentId, courseId }: StudentAssessmentsByCourse): Promise<Assessment | null>
   abstract findManyByStudentIdAndCourseId({ studentId, courseId }: StudentAssessmentsByCourse): Promise<Assessment[]>
-  abstract create(assesment: Assessment): Promise<void>
-  abstract createMany(assesments: Assessment[]): Promise<void>
-  abstract update(assesment: Assessment): Promise<void>
-  abstract delete(assesment: Assessment): Promise<void>
+  abstract create(assessment: Assessment): Promise<void>
+  abstract createMany(assessments: Assessment[]): Promise<void>
+  abstract update(assessment: Assessment): Promise<void>
+  abstract delete(assessment: Assessment): Promise<void>
 }
