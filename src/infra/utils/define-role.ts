ea60b71824074f2661dea7430870d68574b76555
igorabreu29 <abreusense@gmail.com>
2024-07-24 16:58:17 -0300

    feat: add function to different access layer

--- src/infra/utils/define-role.ts
+++ src/infra/utils/define-role.ts
@@ -1,9 +1,16 @@
 import { Role } from "@/domain/boletim/enterprise/entities/authenticate.ts";
 import { Role as PrismaRole } from "@prisma/client";
 
-export function defineRoleAccess(role: PrismaRole): Role {
+export function defineRoleAccessToPrisma(role: PrismaRole): Role {
   if (role === 'DEV') return 'dev'
   if (role === 'ADMIN') return 'admin'
   if (role === 'MANAGER') return 'manager'
   return 'student'
+}
+
+export function defineRoleAccessToDomain(role: Role): PrismaRole {
+  if (role === 'dev') return "DEV"
+  if (role === 'admin') return 'ADMIN'
+  if (role === 'manager') return "MANAGER"
+  return 'STUDENT'
 }
