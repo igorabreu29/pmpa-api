1767214f92f623f7dce68522fe4bee87764432a0
igorabreu29 <abreusense@gmail.com>
2024-07-25 16:42:26 -0300

    refactor: change birthday transform

--- src/infra/http/controllers/create-student.ts
+++ src/infra/http/controllers/create-student.ts
@@ -1,20 +1,17 @@
-import { InvalidCredentialsError } from "@/domain/boletim/app/use-cases/errors/invalid-credentials-error.ts";
-import { makeAuthenticateUseCase } from "@/infra/factories/make-authenticate-use-case.ts";
+import { InvalidBirthdayError } from "@/core/errors/domain/invalid-birthday.ts";
+import { InvalidCPFError } from "@/core/errors/domain/invalid-cpf.ts";
+import { InvalidEmailError } from "@/core/errors/domain/invalid-email.ts";
+import { InvalidNameError } from "@/core/errors/domain/invalid-name.ts";
+import { InvalidPasswordError } from "@/core/errors/domain/invalid-password.ts";
+import { ResourceAlreadyExistError } from "@/core/errors/use-case/resource-already-exist-error.ts";
+import { ResourceNotFoundError } from "@/core/errors/use-case/resource-not-found-error.ts";
+import { makeCreateStudentUseCase } from "@/infra/factories/make-create-student-use-case.ts";
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
 import { ClientError } from "../errors/client-error.ts";
 import { ConflictError } from "../errors/conflict-error.ts";
-import { env } from "@/infra/env/index.ts";
-import { makeCreateStudentUseCase } from "@/infra/factories/make-create-student-use-case.ts";
-import { ResourceNotFoundError } from "@/core/errors/use-case/resource-not-found-error.ts";
 import { NotFound } from "../errors/not-found.ts";
-import { InvalidEmailError } from "@/core/errors/domain/invalid-email.ts";
-import { InvalidPasswordError } from "@/core/errors/domain/invalid-password.ts";
-import { InvalidBirthdayError } from "@/core/errors/domain/invalid-birthday.ts";
-import { InvalidNameError } from "@/core/errors/domain/invalid-name.ts";
-import { InvalidCPFError } from "@/core/errors/domain/invalid-cpf.ts";
-import { ResourceAlreadyExistError } from "@/core/errors/use-case/resource-already-exist-error.ts";
 
 export async function createStudent(
   app: FastifyInstance
@@ -25,7 +22,14 @@ export async function createStudent(
         username: z.string().min(3).max(50),
         email: z.string().email(),
         cpf: z.string().min(14).max(14),
-        birthday: z.coerce.date(),
+        birthday: z.string().transform(birthday => {
+          const [day, month, year] = birthday.split('/')
+
+          const date = new Date()
+          date.setFullYear(Number(year), Number(month), Number(day))
+
+          return date
+        }),
         civilId: z.number(),
         courseId: z.string().cuid(),
         poleId: z.string().cuid()
@@ -41,7 +45,7 @@ export async function createStudent(
       cpf,
       email,
       birthday,
-      civilID: civilId,
+      civilId,
       courseId,
       poleId
     })
@@ -62,10 +66,12 @@ export async function createStudent(
           throw new ConflictError('This name is not valid.') 
         case InvalidCPFError:
           throw new ConflictError('This cpf is not valid.') 
+        case InvalidBirthdayError:
+          throw new ConflictError('This date is not valid.') 
         case ResourceAlreadyExistError: 
-          throw new ResourceAlreadyExistError(error.message)
+          throw new ConflictError('Student already be present in the platform')
         default: 
-          throw new ClientError('Bad Request')
+          throw new ClientError('Ocurred something problem')
       }
     }
 
