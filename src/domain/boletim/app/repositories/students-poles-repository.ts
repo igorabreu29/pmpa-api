95eb5c50036331410b42304dd782e2439bc9c2d8
igorabreu29 <abreusense@gmail.com>
2024-07-29 17:43:40 -0300

    feat: add search students by pole

--- src/domain/boletim/app/repositories/students-poles-repository.ts
+++ src/domain/boletim/app/repositories/students-poles-repository.ts
@@ -1,14 +1,29 @@
 import { StudentPole } from "../../enterprise/entities/student-pole.ts";
+import { StudentCourseDetails } from "../../enterprise/entities/value-objects/student-course-details.ts";
 import { StudentWithPole } from "../../enterprise/entities/value-objects/student-with-pole.ts";
+
+export interface SearchManyDetailsByPole {
+  poleId: string
+  query: string
+  page: number
+}
 
 export abstract class StudentsPolesRepository {
   abstract findByStudentId({ studentId }: { studentId: string }): Promise<StudentPole | null>
+
   abstract findByStudentIdAndPoleId({ studentId, poleId }: { studentId: string, poleId: string }): Promise<StudentPole | null>
   abstract findManyByPoleId({ poleId, page, perPage }: { poleId: string, page?: number, perPage?: number }): Promise<{
     studentsPole: StudentWithPole[],
     pages: number
     totalItems: number
   }>
+
+  abstract searchManyDetailsByPoleId({
+    poleId,
+    query,
+    page
+  }: SearchManyDetailsByPole): Promise<StudentCourseDetails[]>
+  
   abstract create(studentPole: StudentPole): Promise<void>
   abstract createMany(studentsPoles: StudentPole[]): Promise<void>
 }
