5028ceab7c6f3b7da2d25f94ebd4f6562d065380
igorabreu29 <abreusense@gmail.com>
2024-07-29 10:38:05 -0300

    feat: add name validate

--- test/factories/make-discipline.ts
+++ test/factories/make-discipline.ts
@@ -1,12 +1,20 @@
 import { UniqueEntityId } from "@/core/entities/unique-entity-id.ts";
 import { Discipline } from "@/domain/boletim/enterprise/entities/discipline.ts";
+import { Name } from "@/domain/boletim/enterprise/entities/value-objects/name.ts";
+import { faker } from "@faker-js/faker";
 
 export function makeDiscipline(
   override: Partial<Discipline> = {},
   id?: UniqueEntityId
 ) {
-  return Discipline.create({
-    name: 'Instrução Militar',
+  const nameOrError = Name.create(faker.company.name())
+  if (nameOrError.isLeft()) throw new Error(nameOrError.value.message)
+
+  const disciplineOrError = Discipline.create({
+    name: nameOrError.value,
     ...override
   }, id)
+  if (disciplineOrError.isLeft()) throw new Error(disciplineOrError.value.message)
+
+  return disciplineOrError.value
 }
