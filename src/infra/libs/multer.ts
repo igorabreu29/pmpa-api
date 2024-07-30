c0e057c548acadf8b69d4b3019b443bd6ef7e78f
igorabreu29 <abreusense@gmail.com>
2024-07-30 16:50:10 -0300

    feat: add config multer

---
+++ src/infra/libs/multer.ts
@@ -0,0 +1,35 @@
+import multer from 'fastify-multer'
+import { randomUUID } from 'node:crypto'
+import { resolve } from 'node:path'
+
+export const upload = multer({
+    dest: resolve(import.meta.dirname, '../../../uploads'),
+    storage: multer.diskStorage({
+        destination: (req, file, callback) => {
+            callback(null, resolve(import.meta.dirname, '../../../uploads'))
+        },
+        filename: (req, file, callback) => {
+            const fileName = `${randomUUID()}-${file.originalname}`
+            callback(null, fileName)
+        },
+    }),
+    limits: {
+        fileSize: 5 * 1024 * 1024, // 5MB
+    },
+    fileFilter: (req, file, callback) => {
+        const allowedMimes = [
+            'image/jpeg',
+            'image/png',
+            'image/pjpeg',
+            'image/gif',
+            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        ]
+
+        if (!allowedMimes.includes(file.mimetype)) {
+            callback(new Error('Invalid form'))
+            return
+        }
+
+        callback(null, true)
+    }
+})
