c681ffb8995b4d7ee2faa4ad0b7d6d536b5a3ad7
igorabreu29 <abreusense@gmail.com>
2024-07-30 16:45:51 -0300

    refactor: add name validate

--- src/infra/database/mappers/prisma-disciplines-mapper.ts
+++ src/infra/database/mappers/prisma-disciplines-mapper.ts
@@ -1,20 +1,25 @@
 import { UniqueEntityId } from '@/core/entities/unique-entity-id.ts';
 import { Discipline } from '@/domain/boletim/enterprise/entities/discipline.ts';
+import { Name } from '@/domain/boletim/enterprise/entities/value-objects/name.ts';
 import { Prisma, Discipline as PrismaDiscipline } from '@prisma/client';
 
 export class PrismaDisciplinesMapper {
-  static toDomain(course: PrismaDiscipline): Discipline {
-    const discipline = Discipline.create({
-      name: course.name
-    }, new UniqueEntityId(course.id))
+  static toDomain(discipline: PrismaDiscipline): Discipline {
+    const nameOrError = Name.create(discipline.name)
+    if (nameOrError.isLeft()) throw new Error(nameOrError.value.message)
+
+    const disciplineOrError = Discipline.create({
+      name: nameOrError.value
+    }, new UniqueEntityId(discipline.id))
+    if (disciplineOrError.isLeft()) throw new Error(disciplineOrError.value.message)
     
-    return discipline
+    return disciplineOrError.value
   }
 
   static toPrisma(discipline: Discipline): Prisma.PoleUncheckedCreateInput {
     return {
       id: discipline.id.toValue(),
-      name: discipline.name,
+      name: discipline.name.value,
     }
   }
 }
