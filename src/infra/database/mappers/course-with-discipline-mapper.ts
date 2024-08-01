b3bb6879216547c8415c4000245141880eb80a1f
igorabreu29 <abreusense@gmail.com>
2024-08-01 13:02:51 -0300

    feat: create course with discipline mapper

---
+++ src/infra/database/mappers/course-with-discipline-mapper.ts
@@ -0,0 +1,18 @@
+import { UniqueEntityId } from "@/core/entities/unique-entity-id.ts"
+import { CourseWithDiscipline } from "@/domain/boletim/enterprise/entities/value-objects/course-with-discipline.ts"
+import { CourseOnDiscipline as PrismaCourse, Discipline as PrismaDiscipline } from "@prisma/client"
+
+type PrismaCourseWithDiscipline = PrismaCourse & {
+  discipline: PrismaDiscipline
+}
+
+export class PrismaCourseWithDisciplineMapper {
+  static toDomain(courseWithDiscipline: PrismaCourseWithDiscipline): CourseWithDiscipline {
+    return CourseWithDiscipline.create({
+      courseId: new UniqueEntityId(courseWithDiscipline.courseId),
+      disciplineId: new UniqueEntityId(courseWithDiscipline.discipline.id),
+      disciplineName: courseWithDiscipline.discipline.name,
+      module: courseWithDiscipline.module
+    })
+  }
+}
