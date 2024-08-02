1595d56a158c4ffe657ebaaaed0db88d5f87ab32
igorabreu29 <abreusense@gmail.com>
2024-08-02 12:01:21 -0300

    feat: create behaviors batch

---
+++ src/infra/database/repositories/prisma-behaviors-batch-repository.ts
@@ -0,0 +1,13 @@
+import { BehaviorsBatchRepository } from "@/domain/boletim/app/repositories/behaviors-batch-repository.ts";
+import { BehaviorBatch } from "@/domain/boletim/enterprise/entities/behavior-batch.ts";
+import { PrismaBehaviorsMapper } from "../mappers/prisma-behaviors-mapper.ts";
+import { prisma } from "../lib/prisma.ts";
+
+export class PrismaBehaviorsBatchRepository implements BehaviorsBatchRepository {
+  async create(behaviorBatch: BehaviorBatch): Promise<void> {
+    const prismaMapper = behaviorBatch.behaviors.map(behavior => PrismaBehaviorsMapper.toPrisma(behavior))
+    await prisma.behavior.createMany({
+      data: prismaMapper
+    })
+  }
+}
