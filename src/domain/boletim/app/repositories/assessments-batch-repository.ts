849dffe5cabe9f2f0fa1f914b319dd48598d7cea
igorabreu29 <abreusense@gmail.com>
2024-08-06 21:45:31 -0300

    feat: add save method

--- src/domain/boletim/app/repositories/assessments-batch-repository.ts
+++ src/domain/boletim/app/repositories/assessments-batch-repository.ts
@@ -2,4 +2,5 @@ import { AssessmentBatch } from "../../enterprise/entities/assessment-batch.ts";
 
 export abstract class AssessmentsBatchRepository {
   abstract create(assessmentBatch: AssessmentBatch): Promise<void>
+  abstract save(assessmentBatch: AssessmentBatch): Promise<void>
 }
--- test/repositories/in-memory-assessments-batch-repository.ts
+++ test/repositories/in-memory-assessments-batch-repository.ts
@@ -1,13 +1,33 @@
 import { DomainEvents } from "@/core/events/domain-events.ts";
 import { AssessmentsBatchRepository } from "@/domain/boletim/app/repositories/assessments-batch-repository.ts";
 import { AssessmentBatch } from "@/domain/boletim/enterprise/entities/assessment-batch.ts";
+import type { InMemoryAssessmentsRepository } from "./in-memory-assessments-repository.ts";
 
 export class InMemoryAssessmentsBatchRepository implements AssessmentsBatchRepository {
   public items: AssessmentBatch[] = []
+
+  constructor(
+    private assessmentsRepository: InMemoryAssessmentsRepository
+  ) {}
 
   async create(assessmentBatch: AssessmentBatch): Promise<void> {
     this.items.push(assessmentBatch)
 
     DomainEvents.dispatchEventsForAggregate(assessmentBatch.id)
+  }
+
+  async save(assessmentBatch: AssessmentBatch): Promise<void> {
+    this.items.push(assessmentBatch)
+
+    const assessmentBatchIndex = this.items.findIndex(item => item.id.equals(assessmentBatch.id))
+    this.items[assessmentBatchIndex] = assessmentBatch
+
+    console.log(this.assessmentsRepository.items)
+    console.log(assessmentBatch.assessments)
+
+    assessmentBatch.assessments.forEach(assessment => {{
+      const assessmentIndex = this.assessmentsRepository.items.findIndex(item => item.id.equals(assessment.id))
+      this.assessmentsRepository.items[assessmentIndex] = assessment
+    }})
   }
 }
