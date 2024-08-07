1e67eeddd207cb15c87a0f164391fc7dba3a9b38
igorabreu29 <abreusense@gmail.com>
2024-08-07 11:47:35 -0300

    feat: add save method

--- test/repositories/in-memory-behaviors-batch-repository.ts
+++ test/repositories/in-memory-behaviors-batch-repository.ts
@@ -1,12 +1,31 @@
 import { DomainEvents } from "@/core/events/domain-events.ts";
 import { BehaviorsBatchRepository } from "@/domain/boletim/app/repositories/behaviors-batch-repository.ts";
 import { BehaviorBatch } from "@/domain/boletim/enterprise/entities/behavior-batch.ts";
+import { InMemoryBehaviorsRepository } from "./in-memory-behaviors-repository.ts";
 
 export class InMemoryBehaviorsBatchRepository implements BehaviorsBatchRepository {
   public items: BehaviorBatch[] = []
 
+  constructor(
+    private behaviorsRepository: InMemoryBehaviorsRepository
+  ) {}
+
   async create(behaviorBatch: BehaviorBatch): Promise<void> {
     this.items.push(behaviorBatch)
+
+    DomainEvents.dispatchEventsForAggregate(behaviorBatch.id)
+  }
+
+  async save(behaviorBatch: BehaviorBatch): Promise<void> {
+    this.items.push(behaviorBatch)
+
+    const behaviorBatchIndex = this.items.findIndex(item => item.id.equals(behaviorBatch.id))
+    this.items[behaviorBatchIndex] = behaviorBatch
+
+    behaviorBatch.behaviors.forEach(behavior => {{
+      const behaviorIndex = this.behaviorsRepository.items.findIndex(item => item.id.equals(behavior.id))
+      this.behaviorsRepository.items[behaviorIndex] = behavior
+    }})
 
     DomainEvents.dispatchEventsForAggregate(behaviorBatch.id)
   }
