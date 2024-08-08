0864722e88c082d8604573780a35f61161742265
igorabreu29 <abreusense@gmail.com>
2024-08-08 10:27:56 -0300

    refactor: add missing dependencies

--- src/infra/factories/make-update-manager-use-case.ts
+++ src/infra/factories/make-update-manager-use-case.ts
@@ -1,10 +1,16 @@
+import { PrismaManagersCoursesRepository } from "../database/repositories/prisma-managers-courses-repository.ts";
+import { PrismaManagersPolesRepository } from "../database/repositories/prisma-managers-poles-repository.ts";
 import { PrismaManagersRepository } from "../database/repositories/prisma-managers-repository.ts";
 import { UpdateManagerUseCase } from "@/domain/boletim/app/use-cases/update-manager.ts";
 
 export function makeUpdateManagerUseCase() {
   const managersRepository = new PrismaManagersRepository()
+  const managerCoursesRepository = new PrismaManagersCoursesRepository()
+  const managerPolesRepository = new PrismaManagersPolesRepository()
 
   return new UpdateManagerUseCase (
     managersRepository,
+    managerCoursesRepository,
+    managerPolesRepository
   )
 }
