697288aa8ccbbb336c73331c59ed7e8a53c3c755
igorabreu29 <abreusense@gmail.com>
2024-08-08 12:45:04 -0300

    refactor: change functions to get classification

--- src/domain/boletim/app/utils/generate-course-classification.ts
+++ src/domain/boletim/app/utils/generate-course-classification.ts
@@ -1,16 +1,17 @@
 import { StudentClassficationByModule, StudentClassficationByPeriod } from "../types/generate-students-classification.js";
+import { classifyStudentsByCFPFormula, classifyStudentsByCGSAndCASFormula, classifyStudentsByPeriodFormula } from "./classification/classify-students-by-formula.ts";
 
 export const classificationByCourseFormula = {
   CGS: (studentsWithAverage: StudentClassficationByModule[]) => {
-    return classifyStudentsByModuleFormula(studentsWithAverage)
+    return classifyStudentsByCGSAndCASFormula(studentsWithAverage)
   },
 
   CAS: (studentsWithAverage: StudentClassficationByModule[]) => {
-    return classifyStudentsByModuleFormula(studentsWithAverage)
+    return classifyStudentsByCFPFormula(studentsWithAverage)
   },
 
   CFP: (studentsWithAverage: StudentClassficationByModule[]) => {
-    return classifyStudentsByModuleFormula(studentsWithAverage)
+    return classifyStudentsByCFPFormula(studentsWithAverage)
   },
 
   CFO: (studentsWithAverage: StudentClassficationByPeriod[]) => {
@@ -21,76 +22,3 @@ export const classificationByCourseFormula = {
     return classifyStudentsByPeriodFormula(studentsWithAverage)
   },
 } 
-
-const classifyStudentsByPeriodFormula = (studentsWithAverage: StudentClassficationByPeriod[]) => {
-    return studentsWithAverage.sort((studentA, studentB) => {
-      const studentAIsRecoveringInTheThirdModule = studentA.studentAverage.assessments['module3']?.some(student => student.isRecovering)
-      const studentBIsRecoveringInTheThirdModule = studentB.studentAverage.assessments['module3']?.some(student => student.isRecovering)
-
-      const geralAverageStudentA = studentA.studentAverage.averageInform.geralAverage
-      const geralAverageStudentB = studentB.studentAverage.averageInform.geralAverage
-
-      const totalFromStudentAThatIsRecoveringInModule1 = studentA.studentAverage.assessments['module1']?.filter(student => student.average < 7) 
-      const totalFromStudentAThatIsRecoveringInModule2 = studentA.studentAverage.assessments['module2']?.filter(student => student.average < 7) 
-      const totalFromStudentAThatIsRecoveringInModule3 = studentA.studentAverage.assessments['module3']?.filter(student => student.average < 7) 
-
-      const totalFromStudentBThatIsRecoveringInModule1 = studentB.studentAverage.assessments['module1']?.filter(student => student.average < 7) 
-      const totalFromStudentBThatIsRecoveringInModule2 = studentB.studentAverage.assessments['module2']?.filter(student => student.average < 7) 
-      const totalFromStudentBThatIsRecoveringInModule3 = studentB.studentAverage.assessments['module3']?.filter(student => student.average < 7) 
-
-      const totalRecoveringFromStudentA = 
-        totalFromStudentAThatIsRecoveringInModule1?.length + totalFromStudentAThatIsRecoveringInModule2?.length + totalFromStudentAThatIsRecoveringInModule3?.length
-
-      const totalRecoveringFromStudentB = 
-        totalFromStudentBThatIsRecoveringInModule1?.length + totalFromStudentBThatIsRecoveringInModule2?.length + totalFromStudentBThatIsRecoveringInModule3?.length
-
-      const studentABirthday = Number(studentA.studentBirthday?.getTime())
-      const studentBBirthday = Number(studentB.studentBirthday?.getTime())
-
-      if (!studentAIsRecoveringInTheThirdModule && !studentBIsRecoveringInTheThirdModule) {
-        if (geralAverageStudentA < geralAverageStudentB) return 1
-        if (geralAverageStudentA > geralAverageStudentB) return -1
-        if (totalRecoveringFromStudentA < totalRecoveringFromStudentB) return 1
-        if (totalRecoveringFromStudentA > totalRecoveringFromStudentB) return -1
-        if (studentABirthday < studentBBirthday) return 1
-        if (studentABirthday > studentBBirthday) return -1
-      }
-
-      if (geralAverageStudentA < geralAverageStudentB) return 1
-      if (geralAverageStudentA > geralAverageStudentB) return -1
-      if (totalRecoveringFromStudentA < totalRecoveringFromStudentB) return -1
-      if (totalRecoveringFromStudentA > totalRecoveringFromStudentB) return 1
-      if (studentABirthday < studentBBirthday) return -1
-      if (studentABirthday > studentBBirthday) return 1
-
-      return 0
-    })
-}
-
-const classifyStudentsByModuleFormula = (studentsWithAverage: StudentClassficationByModule[]) => {
-  return studentsWithAverage.sort((studentA, studentB) => {
-    const geralAverageStudentA = studentA.studentAverage.averageInform.geralAverage
-    const geralAverageStudentB = studentB.studentAverage.averageInform.geralAverage
-
-    const studentADisicplineAverageIsNotRecovering = studentA.studentAverage.assessments.filter(assessment => {
-      return assessment.average >= 7 && assessment.status !== 'second season' && !assessment.isRecovering
-    })
-    const studentBDisicplineAverageIsNotRecovering = studentA.studentAverage.assessments.filter(assessment => {
-      return assessment.average >= 7 && assessment.status !== 'second season' && !assessment.isRecovering
-    })
-
-    const studentABirthday = Number(studentA.studentBirthday?.getTime())
-    const studentBBirthday = Number(studentB.studentBirthday?.getTime())
-
-    if (geralAverageStudentA < geralAverageStudentB) return 1
-    if (geralAverageStudentA > geralAverageStudentB) return -1
-
-    if (studentADisicplineAverageIsNotRecovering?.length < studentBDisicplineAverageIsNotRecovering?.length) return 1
-    if (studentADisicplineAverageIsNotRecovering?.length > studentBDisicplineAverageIsNotRecovering?.length) return -1
-
-    if (studentABirthday < studentBBirthday) return -1
-    if (studentABirthday > studentBBirthday) return 1
-
-    return 0
-  })
-}
