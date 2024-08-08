33c702f9f5915c9ad38109a8d40803c792ec402e
igorabreu29 <abreusense@gmail.com>
2024-08-08 12:45:41 -0300

    feat: create classify studens by formula utils

---
+++ src/domain/boletim/app/utils/classification/classify-students-by-formula.spec.ts
@@ -0,0 +1,238 @@
+import { describe, expect, it } from "vitest";
+import { Status } from "../get-assessment-average-status.ts";
+import { GenerateBehaviorStatus } from "../get-behavior-average-status.ts";
+import { StudentClassficationByModule } from "../../types/generate-students-classification.js";
+import { generateBehaviorAverage } from "../generate-behavior-average.ts";
+import { classifyStudentsByCFPFormula } from "./classify-students-by-formula.ts";
+
+describe('Classify Students By Formula', () => {
+  it.skip ('should be able to classify students by CFP formula', () => {
+    const behaviorMonths = [
+      {
+        august: 7,
+        september: 7,
+        october: 7,
+        november: 8,
+        december: 8,
+      },
+      {
+        january: 9.5,
+        february: 7.5,
+        march: 6.2,
+      },
+    ]
+
+    const behaviorAverageByPeriod = generateBehaviorAverage({ behaviorMonths })
+
+    const studentsWithAverage: StudentClassficationByModule[] = [
+      {
+        studentAverage: {
+          assessments: [
+            {
+              id: 'assessment-1',
+              vf: 9,
+              avi: 7,
+              avii: 8,
+              module: 1,
+              average: 8,
+              isRecovering: false,
+              status: 'approved' as Status
+            },
+            {
+              id: 'assessment-2',
+              vf: 7,
+              avi: 5,
+              avii: 8,
+              module: 1,
+              average: 6.67,
+              isRecovering: false,
+              status: 'second season' as Status
+            },
+          ],
+          averageInform: {
+            behaviorAverageStatus: behaviorAverageByPeriod.behaviorAverageStatus as GenerateBehaviorStatus[],
+            geralAverage: 6.500,
+            studentAverageStatus: {
+              concept: 'regular',
+              status: 'approved second season'
+            },
+            behaviorsCount: 8
+          },
+          assessmentsCount: 6
+        },
+        studentBirthday: new Date(2001, 5, 12)
+      },
+      {
+        studentAverage: {
+          assessments: [
+            {
+              id: 'assessment-3',
+              vf: 7,
+              avi: 5,
+              avii: 8,
+              module: 1,
+              average: 6.67,
+              isRecovering: false,
+              status: 'second season' as Status
+            },
+            {
+              id: 'assessment-4',
+              vf: 7,
+              avi: 5,
+              avii: 8,
+              module: 1,
+              average: 6.67,
+              isRecovering: false,
+              status: 'second season' as Status
+            },
+          ],
+          averageInform: {
+            behaviorAverageStatus: behaviorAverageByPeriod.behaviorAverageStatus as GenerateBehaviorStatus[],
+            geralAverage: 6.500,
+            studentAverageStatus: {
+              concept: 'regular',
+              status: 'approved second season'
+            },
+            behaviorsCount: 8
+          },
+          assessmentsCount: 6
+        },
+        studentBirthday: new Date(1990, 5, 12)
+      }
+    ]
+    
+    const result = classifyStudentsByCFPFormula(studentsWithAverage)
+    
+  })
+
+  it ('should be able to classify students by CGS And CAS formula', () => {
+    const behaviorMonths = [
+      {
+        august: 7,
+        september: 7,
+        october: 7,
+        november: 8,
+        december: 8,
+      },
+      {
+        january: 9.5,
+        february: 7.5,
+        march: 6.2,
+      },
+    ]
+
+    const behaviorAverageByPeriod = generateBehaviorAverage({ behaviorMonths })
+
+    const studentsWithAverage: StudentClassficationByModule[] = [
+      {
+        studentAverage: {
+          assessments: [
+            {
+              id: 'assessment-1',
+              vf: 9,
+              avi: 7,
+              avii: 8,
+              module: 1,
+              average: 8,
+              isRecovering: false,
+              status: 'approved' as Status
+            },
+            {
+              id: 'assessment-2',
+              vf: 7,
+              avi: 5,
+              avii: 8,
+              module: 1,
+              average: 6.67,
+              isRecovering: false,
+              status: 'second season' as Status
+            },
+          ],
+          averageInform: {
+            behaviorAverageStatus: behaviorAverageByPeriod.behaviorAverageStatus as GenerateBehaviorStatus[],
+            geralAverage: 6.500,
+            studentAverageStatus: {
+              concept: 'regular',
+              status: 'approved second season'
+            },
+            behaviorsCount: 8
+          },
+          assessmentsCount: 6
+        },
+        studentBirthday: new Date(2001, 5, 12)
+      },
+      {
+        studentAverage: {
+          assessments: [
+            {
+              id: 'assessment-3',
+              vf: 7,
+              avi: 5,
+              avii: 8,
+              module: 1,
+              average: 6.67,
+              isRecovering: false,
+              status: 'second season' as Status
+            },
+            {
+              id: 'assessment-4',
+              vf: 7,
+              avi: 5,
+              avii: 8,
+              module: 1,
+              average: 6.67,
+              isRecovering: false,
+              status: 'second season' as Status
+            },
+          ],
+          averageInform: {
+            behaviorAverageStatus: behaviorAverageByPeriod.behaviorAverageStatus as GenerateBehaviorStatus[],
+            geralAverage: 6.500,
+            studentAverageStatus: {
+              concept: 'regular',
+              status: 'approved second season'
+            },
+            behaviorsCount: 8
+          },
+          assessmentsCount: 6
+        },
+        studentBirthday: new Date(1990, 5, 12)
+      }
+    ]
+
+    const result = classifyStudentsByCFPFormula(studentsWithAverage)
+    // console.log(result[0].studentAverage.assessments)
+    // console.log(result[1].studentAverage.assessments)
+
+    expect(result).toMatchObject([
+      {
+        studentAverage: {
+          assessments: [
+            {
+              id: 'assessment-1',
+              status: 'approved'
+            },
+            {
+              id: 'assessment-2',
+              status: 'second season'
+            },
+          ]
+        },
+      },
+      {
+        studentAverage: {
+          assessments: [
+            {
+              id: 'assessment-3',
+              status: 'second season'
+            },
+            {
+              id: 'assessment-4',
+              status: 'second season'
+            },
+          ]
+        }
+      },
+    ])
+  })
+})
---
+++ src/domain/boletim/app/utils/classification/classify-students-by-formula.ts
@@ -0,0 +1,95 @@
+import { StudentClassficationByModule, StudentClassficationByPeriod } from "../../types/generate-students-classification.js"
+
+export const classifyStudentsByPeriodFormula = (studentsWithAverage: StudentClassficationByPeriod[]) => {
+  return studentsWithAverage.sort((studentA, studentB) => {
+    const studentAIsRecoveringInTheThirdModule = studentA.studentAverage.assessments['module3']?.some(student => student.isRecovering)
+    const studentBIsRecoveringInTheThirdModule = studentB.studentAverage.assessments['module3']?.some(student => student.isRecovering)
+
+    const geralAverageStudentA = studentA.studentAverage.averageInform.geralAverage
+    const geralAverageStudentB = studentB.studentAverage.averageInform.geralAverage
+
+    const totalFromStudentAThatIsRecoveringInModule1 = studentA.studentAverage.assessments['module1']?.filter(student => student.average < 7) 
+    const totalFromStudentAThatIsRecoveringInModule2 = studentA.studentAverage.assessments['module2']?.filter(student => student.average < 7) 
+    const totalFromStudentAThatIsRecoveringInModule3 = studentA.studentAverage.assessments['module3']?.filter(student => student.average < 7) 
+
+    const totalFromStudentBThatIsRecoveringInModule1 = studentB.studentAverage.assessments['module1']?.filter(student => student.average < 7) 
+    const totalFromStudentBThatIsRecoveringInModule2 = studentB.studentAverage.assessments['module2']?.filter(student => student.average < 7) 
+    const totalFromStudentBThatIsRecoveringInModule3 = studentB.studentAverage.assessments['module3']?.filter(student => student.average < 7) 
+
+    const totalRecoveringFromStudentA = 
+      totalFromStudentAThatIsRecoveringInModule1?.length + totalFromStudentAThatIsRecoveringInModule2?.length + totalFromStudentAThatIsRecoveringInModule3?.length
+
+    const totalRecoveringFromStudentB = 
+      totalFromStudentBThatIsRecoveringInModule1?.length + totalFromStudentBThatIsRecoveringInModule2?.length + totalFromStudentBThatIsRecoveringInModule3?.length
+
+    const studentABirthday = Number(studentA.studentBirthday?.getTime())
+    const studentBBirthday = Number(studentB.studentBirthday?.getTime())
+
+    if (!studentAIsRecoveringInTheThirdModule && !studentBIsRecoveringInTheThirdModule) {
+      if (geralAverageStudentA < geralAverageStudentB) return 1
+      if (geralAverageStudentA > geralAverageStudentB) return -1
+      if (totalRecoveringFromStudentA < totalRecoveringFromStudentB) return 1
+      if (totalRecoveringFromStudentA > totalRecoveringFromStudentB) return -1
+      if (studentABirthday < studentBBirthday) return 1
+      if (studentABirthday > studentBBirthday) return -1
+    }
+
+    if (geralAverageStudentA < geralAverageStudentB) return 1
+    if (geralAverageStudentA > geralAverageStudentB) return -1
+    if (totalRecoveringFromStudentA < totalRecoveringFromStudentB) return -1
+    if (totalRecoveringFromStudentA > totalRecoveringFromStudentB) return 1
+    if (studentABirthday < studentBBirthday) return -1
+    if (studentABirthday > studentBBirthday) return 1
+
+    return 0
+  })
+}
+
+export const classifyStudentsByCFPFormula = (studentsWithAverage: StudentClassficationByModule[]) => {
+  return studentsWithAverage.sort((studentA, studentB) => {
+    const geralAverageStudentA = studentA.studentAverage.averageInform.geralAverage
+    const geralAverageStudentB = studentB.studentAverage.averageInform.geralAverage
+
+    const studentABirthday = Number(studentA.studentBirthday?.getTime())
+    const studentBBirthday = Number(studentB.studentBirthday?.getTime())
+
+    if (geralAverageStudentA < geralAverageStudentB) return 1
+    if (geralAverageStudentA > geralAverageStudentB) return -1
+
+    if (studentABirthday < studentBBirthday) return -1
+    if (studentABirthday > studentBBirthday) return 1
+
+    return 0
+  })
+}
+
+export const classifyStudentsByCGSAndCASFormula = (studentsWithAverage: StudentClassficationByModule[]) => {
+  return studentsWithAverage.sort((studentA, studentB) => {
+    const geralAverageStudentA = studentA.studentAverage.averageInform.geralAverage
+    const geralAverageStudentB = studentB.studentAverage.averageInform.geralAverage
+
+    console.log(studentA)
+    console.log(studentB)
+
+    const studentAStatusAssessmentEqualApproved = studentA.studentAverage.assessments.filter(assessment => {
+      return assessment.status === 'approved'
+    })
+    const studentBStatusAssessmentEqualApproved = studentB.studentAverage.assessments.filter(assessment => {
+      return assessment.status === 'approved'
+    })
+
+    const studentABirthday = Number(studentA.studentBirthday?.getTime())
+    const studentBBirthday = Number(studentB.studentBirthday?.getTime())
+
+    if (geralAverageStudentA < geralAverageStudentB) return 1
+    if (geralAverageStudentA > geralAverageStudentB) return -1
+
+    if (studentAStatusAssessmentEqualApproved?.length < studentBStatusAssessmentEqualApproved?.length) return 1
+    if (studentAStatusAssessmentEqualApproved?.length > studentBStatusAssessmentEqualApproved?.length) return -1
+
+    if (studentABirthday < studentBBirthday) return -1
+    if (studentABirthday > studentBBirthday) return 1
+
+    return 0
+  })
+}
