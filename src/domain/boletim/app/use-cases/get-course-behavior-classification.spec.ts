68d0543c12e1a2be2f0d297abb9110b3b11b8e93
igorabreu29 <abreusense@gmail.com>
2024-08-10 17:38:56 -0300

    feat: create test and add return to classification

---
+++ src/domain/boletim/app/use-cases/get-course-behavior-classification.spec.ts
@@ -0,0 +1,212 @@
+import { InMemoryBehaviorsRepository } from 'test/repositories/in-memory-behaviors-repository.ts'
+import { InMemoryCoursesRepository } from 'test/repositories/in-memory-courses-repository.ts'
+import { InMemoryPolesRepository } from 'test/repositories/in-memory-poles-repository.ts'
+import { InMemoryStudentsCoursesRepository } from 'test/repositories/in-memory-students-courses-repository.ts'
+import { InMemoryStudentsPolesRepository } from 'test/repositories/in-memory-students-poles-repository.ts'
+import { InMemoryStudentsRepository } from 'test/repositories/in-memory-students-repository.ts'
+import { describe, it, expect, beforeEach } from 'vitest'
+import { GetCourseBehaviorClassification } from './get-course-behavior-classification.ts'
+import { ResourceNotFoundError } from '@/core/errors/use-case/resource-not-found-error.ts'
+import { makeCourse } from 'test/factories/make-course.ts'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id.ts'
+import { makePole } from 'test/factories/make-pole.ts'
+import { makeStudentCourse } from 'test/factories/make-student-course.ts'
+import { makeStudentPole } from 'test/factories/make-student-pole.ts'
+import { makeStudent } from 'test/factories/make-student.ts'
+import { makeBehavior } from 'test/factories/make-behavior.ts'
+
+let studentsRepository: InMemoryStudentsRepository
+let coursesRepository: InMemoryCoursesRepository
+let studentsPolesRepository: InMemoryStudentsPolesRepository
+let polesRepository: InMemoryPolesRepository
+
+let studentsCoursesRepository: InMemoryStudentsCoursesRepository
+let behaviorsRepository: InMemoryBehaviorsRepository
+let sut: GetCourseBehaviorClassification
+
+describe('Get Course Behavior Classification', () => {
+  beforeEach(() => {
+    studentsRepository = new InMemoryStudentsRepository(
+      studentsCoursesRepository,
+      coursesRepository,
+      studentsPolesRepository,
+      polesRepository
+    )
+    coursesRepository = new InMemoryCoursesRepository ()
+    studentsPolesRepository = new InMemoryStudentsPolesRepository(
+      studentsRepository,
+      studentsCoursesRepository,
+      coursesRepository,
+      polesRepository
+    )
+    polesRepository = new InMemoryPolesRepository()
+
+    studentsCoursesRepository = new InMemoryStudentsCoursesRepository(
+      studentsRepository,
+      coursesRepository,
+      studentsPolesRepository,
+      polesRepository
+    )
+    behaviorsRepository = new InMemoryBehaviorsRepository()
+
+    sut = new GetCourseBehaviorClassification(
+      coursesRepository,
+      studentsCoursesRepository,
+      behaviorsRepository
+    )
+  })
+
+  it ('should not be able to get behavior classification if course does not exist', async () => {
+    const result = await sut.execute({
+      courseId: 'not-exist',
+      page: 1
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(ResourceNotFoundError)
+  })
+
+  it ('should be able to get behavior classification with module formula', async () => {
+    const course = makeCourse({ formula: 'CAS' }, new UniqueEntityId('course-1'))
+    coursesRepository.create(course)
+
+    const pole1 = makePole()
+    const pole2 = makePole()
+    polesRepository.createMany([pole1, pole2])
+
+    const student1 = makeStudent({}, new UniqueEntityId('student-1'))
+    const student2 = makeStudent({}, new UniqueEntityId('student-2'))
+    studentsRepository.create(student1)
+    studentsRepository.create(student2)
+
+    const studentCourse1 = makeStudentCourse({ studentId: student1.id, courseId: course.id })
+    const studentCourse2 = makeStudentCourse({ studentId: student2.id, courseId: course.id })
+    studentsCoursesRepository.create(studentCourse1)
+    studentsCoursesRepository.create(studentCourse2)
+
+    const studentPole1 = makeStudentPole({ studentId: studentCourse1.id, poleId: pole1.id })
+    const studentPole2 = makeStudentPole({ studentId: studentCourse2.id, poleId: pole2.id })
+    studentsPolesRepository.create(studentPole1)
+    studentsPolesRepository.create(studentPole2)
+
+    const behavior = makeBehavior({
+      january: 8,
+      february: 8,
+      march: 8,
+      april: 8,
+      studentId: student1.id,
+      courseId: course.id
+    })
+    const behavior2 = makeBehavior({
+      january: 7,
+      february: 9,
+      march: 6,
+      april: 8,
+      studentId: student2.id,
+      courseId: course.id
+    })
+    behaviorsRepository.create(behavior)
+    behaviorsRepository.create(behavior2)
+
+    const result = await sut.execute({
+      courseId: course.id.toValue(),
+      page: 1
+    })
+
+    expect(result.value).toMatchObject({
+      studentsWithBehaviorAverage: [
+        {
+          behaviorAverage: {
+            behaviorAverageStatus: {
+              behaviorAverage: 8,
+              status: 'approved'
+            },
+            behaviorsCount: 4
+          }
+        },
+        {
+          behaviorAverage: {
+            behaviorAverageStatus: {
+              behaviorAverage: 7.5,
+              status: 'approved'
+            },
+            behaviorsCount: 4
+          }
+        },
+      ]
+    })
+  })
+
+  it ('should be able to get behavior classification with period formula', async () => {
+    const course = makeCourse({ formula: 'CFO', isPeriod: true }, new UniqueEntityId('course-1'))
+    coursesRepository.create(course)
+
+    const pole1 = makePole()
+    const pole2 = makePole()
+    polesRepository.createMany([pole1, pole2])
+
+    const student1 = makeStudent({}, new UniqueEntityId('student-1'))
+    const student2 = makeStudent({}, new UniqueEntityId('student-2'))
+    studentsRepository.create(student1)
+    studentsRepository.create(student2)
+
+    const studentCourse1 = makeStudentCourse({ studentId: student1.id, courseId: course.id })
+    const studentCourse2 = makeStudentCourse({ studentId: student2.id, courseId: course.id })
+    studentsCoursesRepository.create(studentCourse1)
+    studentsCoursesRepository.create(studentCourse2)
+
+    const studentPole1 = makeStudentPole({ studentId: studentCourse1.id, poleId: pole1.id })
+    const studentPole2 = makeStudentPole({ studentId: studentCourse2.id, poleId: pole2.id })
+    studentsPolesRepository.create(studentPole1)
+    studentsPolesRepository.create(studentPole2)
+
+    const behavior = makeBehavior({
+      january: 8,
+      february: 8,
+      march: 8,
+      april: 8,
+      studentId: student1.id,
+      courseId: course.id
+    })
+    const behavior2 = makeBehavior({
+      january: 7,
+      february: 9,
+      march: 6,
+      april: 8,
+      studentId: student2.id,
+      courseId: course.id
+    })
+    behaviorsRepository.create(behavior)
+    behaviorsRepository.create(behavior2)
+
+    const result = await sut.execute({
+      courseId: course.id.toValue(),
+      page: 1
+    })
+
+    expect(result.value).toMatchObject({
+      studentsWithBehaviorAverage: [
+        {
+          behaviorAverage: {
+            behaviorAverageStatus: [
+              {
+                behaviorAverage: 8,
+                status: 'approved'
+              }
+            ]
+          },
+        },
+        {
+          behaviorAverage: {
+            behaviorAverageStatus: [
+              {
+                behaviorAverage: 7.5,
+                status: 'approved'
+              }
+            ]
+          },
+        }
+      ]
+    })
+  })
+})
---
+++ src/domain/boletim/app/use-cases/get-course-behavior-classification.ts
@@ -0,0 +1,163 @@
+import { ResourceNotFoundError } from "@/core/errors/use-case/resource-not-found-error.ts"
+import type { CoursesRepository } from "../repositories/courses-repository.ts"
+import type { StudentsCoursesRepository } from "../repositories/students-courses-repository.ts"
+import { left, right } from "@/core/either.ts"
+import type { BehaviorsRepository } from "../repositories/behaviors-repository.ts"
+import { generateBehaviorAverage } from "../utils/generate-behavior-average.ts"
+import type { GenerateBehaviorStatus } from "../utils/get-behavior-average-status.ts"
+import { InvalidCourseFormulaError } from "./errors/invalid-course-formula-error.ts"
+
+interface GetCourseBehaviorClassificationRequest {
+  courseId: string
+  page: number
+}
+
+export interface CourseBehaviorClassificationByModule {
+  behaviorAverage: {
+    behaviorAverageStatus: GenerateBehaviorStatus
+    behaviorsCount: number
+  }
+  studentBirthday: Date
+  studentCivilID: number
+  studentPole: string
+}
+
+export interface CourseBehaviorClassificationByPeriod {
+  behaviorAverage: {
+    behaviorAverageStatus: GenerateBehaviorStatus[]
+    behaviorsCount: number
+  }
+  studentBirthday: Date
+  studentCivilID: number
+  studentPole: string
+}
+
+export class GetCourseBehaviorClassification {
+  constructor (
+    private coursesRepository: CoursesRepository,
+    private studentsCoursesRepository: StudentsCoursesRepository,
+    private behaviorsRepository: BehaviorsRepository
+  ) {}
+
+  async execute({ courseId, page }: GetCourseBehaviorClassificationRequest) {
+    const course = await this.coursesRepository.findById(courseId)
+    if (!course) return left(new ResourceNotFoundError('Course not found.'))
+
+    const { studentsCourse: students } = await this.studentsCoursesRepository.findManyDetailsByCourseId({ courseId, page, perPage: 30 })
+
+    const behaviors = await Promise.all(students.map(async student => {
+      const behaviors = await this.behaviorsRepository.findManyByStudentIdAndCourseId({ studentId: student.studentId.toValue(), courseId })
+      const behaviorMonths = behaviors.map(({
+        january,
+        february,
+        march,
+        april,
+        may,
+        jun,
+        july,
+        august,
+        september,
+        october,
+        november,
+        december,
+      }) => ({
+        january,
+        february,
+        march,
+        april,
+        may,
+        jun,
+        july,
+        august,
+        september,
+        october,
+        november,
+        december,
+      }))
+      
+      const behaviorAverage = generateBehaviorAverage({ behaviorMonths, isPeriod: course.isPeriod })
+
+      return {
+        behaviorAverage,
+        studentBirthday: student.birthday,
+        studentCivilID: student.civilId,
+        studentPole: student.pole
+      }
+    }))
+
+    switch (course.formula) {
+      case 'CGS': 
+        const classifiedByCGSFormula = classifyStudentsByModuleFormula(behaviors as CourseBehaviorClassificationByModule[])
+        return right({ studentsWithBehaviorAverage: classifiedByCGSFormula })
+      case 'CAS': 
+        const classifiedByCASFormula = classifyStudentsByModuleFormula(behaviors as CourseBehaviorClassificationByModule[])
+        return right({ studentsWithBehaviorAverage: classifiedByCASFormula })
+      case 'CFP': 
+        const classifiedByCFPFormula = classifyStudentsByModuleFormula(behaviors as CourseBehaviorClassificationByModule[])
+        return right({ studentsWithBehaviorAverage: classifiedByCFPFormula })
+      case 'CFO': 
+        const classifiedByCFOFormula = classifyStudentsByPeriodFormula(behaviors as CourseBehaviorClassificationByPeriod[])
+        return right({ studentsWithBehaviorAverage: classifiedByCFOFormula })
+      case 'CHO': 
+        const classifiedByCHOFormula = classifyStudentsByPeriodFormula(behaviors as CourseBehaviorClassificationByPeriod[])
+        return right({ studentsWithBehaviorAverage: classifiedByCHOFormula })
+      default: 
+        return left(new InvalidCourseFormulaError(`This formula: ${course.formula} is invalid.`))
+    }
+  }
+}
+
+export const classifyStudentsByPeriodFormula = (behaviorsAverage: CourseBehaviorClassificationByPeriod[]) => {
+  return behaviorsAverage.sort((studentA, studentB) => {
+    const studentAIsRecoveringInTheThirdModule = studentA.behaviorAverage?.behaviorAverageStatus?.some(behavior => behavior.status === 'approved')
+    const studentBIsRecoveringInTheThirdModule = studentB.behaviorAverage?.behaviorAverageStatus?.some(behavior => behavior.status === 'approved')
+
+    const geralAverageStudentA = studentA.behaviorAverage.behaviorAverageStatus?.reduce((acc, behavior) => acc + behavior.behaviorAverage , 0)
+    const geralAverageStudentB = studentB.behaviorAverage.behaviorAverageStatus?.reduce((acc, behavior) => acc + behavior.behaviorAverage, 0)
+
+    const totalFromStudentAThatIsRecovering = studentA.behaviorAverage.behaviorAverageStatus?.filter(student => student.behaviorAverage < 7)
+
+    const totalFromStudentBThatIsRecovering = studentB.behaviorAverage.behaviorAverageStatus?.filter(student => student.behaviorAverage < 7)
+
+    const studentABirthday = Number(studentA.studentBirthday?.getTime())
+    const studentBBirthday = Number(studentB.studentBirthday?.getTime())
+
+    if (!studentAIsRecoveringInTheThirdModule && !studentBIsRecoveringInTheThirdModule) {
+      if (geralAverageStudentA < geralAverageStudentB) return 1
+      if (geralAverageStudentA > geralAverageStudentB) return -1
+      if (totalFromStudentAThatIsRecovering < totalFromStudentBThatIsRecovering) return 1
+      if (totalFromStudentAThatIsRecovering > totalFromStudentBThatIsRecovering) return -1
+      if (studentABirthday < studentBBirthday) return 1
+      if (studentABirthday > studentBBirthday) return -1
+    }
+
+    if (geralAverageStudentA < geralAverageStudentB) return 1
+    if (geralAverageStudentA > geralAverageStudentB) return -1
+    if (totalFromStudentAThatIsRecovering < totalFromStudentBThatIsRecovering) return -1
+    if (totalFromStudentAThatIsRecovering > totalFromStudentBThatIsRecovering) return 1
+    if (studentABirthday < studentBBirthday) return -1
+    if (studentABirthday > studentBBirthday) return 1
+
+    return 0
+  })
+}
+
+export const classifyStudentsByModuleFormula = (behaviorsAverage: CourseBehaviorClassificationByModule[]) => {
+  return behaviorsAverage.sort((studentA, studentB) => {
+    const geralAverageStudentA = studentA.behaviorAverage.behaviorAverageStatus.behaviorAverage
+    const geralAverageStudentB = studentB.behaviorAverage.behaviorAverageStatus.behaviorAverage
+    
+    const studentABirthday = Number(studentA.studentBirthday?.getTime())
+    const studentBBirthday = Number(studentB.studentBirthday?.getTime())
+
+    if (geralAverageStudentA !== geralAverageStudentB) {
+      return Number(geralAverageStudentB) - Number(geralAverageStudentA)
+    }
+
+    if (studentABirthday !== studentBBirthday) {
+      return studentABirthday - studentBBirthday
+    }
+
+    return 0
+  })
+}
