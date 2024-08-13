4378c921ca6e7265758bd422e7408e347c0e7e04
igorabreu29 <abreusense@gmail.com>
2024-08-13 13:04:55 -0300

    feat: change student avatar

---
+++ src/infra/http/controllers/change-student-avatar.e2e-spec.ts
@@ -0,0 +1,63 @@
+import { app } from '@/infra/app.ts'
+import { describe, it, expect, beforeAll, afterAll } from 'vitest'
+
+import request from 'supertest'
+import { prisma } from '@/infra/database/lib/prisma.ts'
+import { transformDate } from '@/infra/utils/transform-date.ts'
+
+import bcrypt from 'bcryptjs'
+
+describe('Change Student Avatar (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  }) 
+
+  afterAll(async () => {
+    await app.close()
+  }) 
+
+  it ('PATCH /students/avatar', async () => {
+    const endsAt = new Date()
+    endsAt.setMinutes(new Date().getMinutes() + 10)
+
+    const student = await prisma.user.create({
+      data: {
+        username: 'John Doe',
+        civilId: '02346',
+        cpf: '12345678911',
+        email: 'july@acne.com', 
+        password: await bcrypt.hash('node-20', 8),
+        role: 'STUDENT',
+        isLoginConfirmed: new Date(),
+        birthday: transformDate('01/04/2001')
+      }
+    })
+
+    const authenticateResponse = await request(app.server)
+      .post('/credentials/auth')
+      .send({
+        cpf: student.cpf,
+        password: 'node-20'
+      })
+    const { token } = authenticateResponse.body
+
+    const response = await request(app.server)
+      .patch(`/students/avatar`)
+      .set('Authorization', `Bearer ${token}`)
+      .attach('avatar', 'test/upload/profile.jpg')
+
+    expect(response.statusCode).toEqual(204)
+
+    const studentUpdated = await prisma.user.findUnique({
+      where: {
+        id: student.id
+      },
+
+      select: {
+        avatarUrl: true
+      }
+    })
+
+    expect(studentUpdated?.avatarUrl).toBeDefined()
+  })
+})
