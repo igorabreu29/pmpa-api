0299e8fa41c1872a418bbe1cea3399c3c04f339c
igorabreu29 <abreusense@gmail.com>
2024-08-19 15:18:14 -0300

    feat: report presenter

---
+++ src/infra/http/presenters/report-presenter.ts
@@ -0,0 +1,17 @@
+import { Report } from '@/domain/report/enterprise/entities/report.ts'
+import { Prisma } from '@prisma/client'
+
+export class ReportPresenter {
+  static toHTTP(report: Report): Prisma.ReportUncheckedCreateInput {
+    return {
+      id: report.id.toValue(),
+      title: report.title,
+      content: report.content,
+      createdAt: report.createdAt,
+      filelink: report.fileLink,
+      filename: report.fileName,
+      ip: report.ip,
+      reporterId: report.reporterId
+    }
+  }
+}
