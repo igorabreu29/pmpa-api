ef52dd07e588aa2dc6636de2cda94d4284605c46
igorabreu29 <abreusense@gmail.com>
2024-08-19 15:14:56 -0300

    feat: add new events

--- src/domain/boletim/enterprise/events/assessment-deleted-event.ts
+++ src/domain/boletim/enterprise/events/assessment-deleted-event.ts
@@ -4,36 +4,23 @@ import { Assessment } from "../entities/assessment.ts";
 
 interface AssessmentEventProps {
   assessment: Assessment
-  courseName: string
-  disciplineName: string
-  studentName: string
   reporterId: string
   reporterIp: string
 }
 
-export class AssessmentEvent implements DomainEvent {
+export class AssessmentDeletedEvent implements DomainEvent {
   public ocurredAt: Date
   public assessment: Assessment
-  
-  public courseName: string
-  public disciplineName: string
-  public studentName: string
 
   public reporterId: string
   public reporterIp: string
 
   public constructor({
     assessment,
-    courseName,
-    disciplineName,
-    studentName,
     reporterId,
     reporterIp
   }: AssessmentEventProps) {
     this.assessment = assessment
-    this.courseName = courseName
-    this.disciplineName = disciplineName
-    this.studentName = studentName
     this.reporterId = reporterId
     this.reporterIp = reporterIp
     this.ocurredAt = new Date()
--- src/domain/boletim/enterprise/events/behavior-updated-event.ts
+++ src/domain/boletim/enterprise/events/behavior-updated-event.ts
@@ -2,27 +2,21 @@ import { UniqueEntityId } from "@/core/entities/unique-entity-id.ts";
 import { DomainEvent } from "@/core/events/domain-event.ts";
 import { Behavior } from "../entities/behavior.ts";
 
-export interface BehaviorEventProps {
+export interface BehaviorUpdatedEventProps {
   behavior: Behavior
-  courseName: string
-  studentName: string
 
   reporterId: string
   reporterIp: string
 }
 
-export class BehaviorEvent implements DomainEvent {
+export class BehaviorUpdatedEvent implements DomainEvent {
   public ocurredAt: Date;
   public behavior: Behavior
-  public courseName: string
-  public studentName: string
   public reporterId: string
   public reporterIp: string
 
-  public constructor({ behavior, courseName, studentName, reporterId, reporterIp }: BehaviorEventProps) {
+  public constructor({ behavior, reporterId, reporterIp }: BehaviorUpdatedEventProps) {
     this.behavior = behavior
-    this.courseName = courseName
-    this.studentName = studentName
     this.reporterId = reporterId
     this.reporterIp = reporterIp
     this.ocurredAt = new Date()
