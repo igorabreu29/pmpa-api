3ed4176afd0e5bcd541d48bd7fdec39b607a2651
igorabreu29 <abreusense@gmail.com>
2024-09-05 16:43:00 -0300

    style: remove unused import

--- src/infra/factories/make-get-course-student-use-case.ts
+++ src/infra/factories/make-get-course-student-use-case.ts
@@ -1,4 +1,3 @@
-import { FetchCourseStudentsUseCase } from "@/domain/boletim/app/use-cases/fetch-course-students.ts";
 import { PrismaCoursesRepository } from "../database/repositories/prisma-courses-repository.ts";
 import { PrismaStudentsCoursesRepository } from "../database/repositories/prisma-students-courses-repository.ts";
 import { GetCourseStudentUseCase } from "@/domain/boletim/app/use-cases/get-course-student.ts";
