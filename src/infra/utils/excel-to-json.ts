88317dcf585b0c3cf2bb479f696433f1ed41f21b
igorabreu <abreusense@gmail.com>
2024-09-10 12:54:31 -0300

    fix: fix merge conflict between previous commits

--- src/infra/utils/excel-to-json.ts
+++ src/infra/utils/excel-to-json.ts
@@ -1,14 +1,7 @@
 import { formatCPF } from '@/core/utils/formatCPF.ts';
-<<<<<<< HEAD
-import xlsx from 'xlsx';
-import { resolve } from 'node:path';
-import { cwd } from 'node:process'
-import fileSystem from 'node:fs'
-=======
 import { join } from 'node:path';
 import { cwd } from 'node:process';
 import { readFile, utils } from '../libs/xlsx.ts'
->>>>>>> 475cc375b3f2ea5b2df4391ba52c8f454b9494a4
 
 export interface ExcelCreateStudentsBatch {
   CPF: number
@@ -16,13 +9,8 @@ export interface ExcelCreateStudentsBatch {
   'E-MAIL': string
   'RG CIVIL': number
   'DATA DE NASCIMENTO': Date
-<<<<<<< HEAD
-  'CURSO': string
-  'POLO': string
-=======
   CURSO: string
   POLO: string
->>>>>>> 475cc375b3f2ea5b2df4391ba52c8f454b9494a4
 }
 
 export interface ExcelUpdateStudentsBatch {
@@ -31,13 +19,8 @@ export interface ExcelUpdateStudentsBatch {
   'E-MAIL'?: string
   'RG CIVIL'?: number
   'DATA DE NASCIMENTO'?: Date
-<<<<<<< HEAD
-  'CURSO'?: string
-  'POLO'?: string
-=======
   CURSO: string
   POLO: string
->>>>>>> 475cc375b3f2ea5b2df4391ba52c8f454b9494a4
 }
 
 export interface ExcelAssessmentsBatch {
@@ -66,87 +49,6 @@ export interface ExcelBehaviorsBatch {
 }
 
 export function createStudentsBatchExcelToJSON(fileUrl: string) {
-<<<<<<< HEAD
-  const workbook = xlsx.readFile(resolve(cwd(), fileUrl))
-
-  const sheets = workbook.SheetNames
-  const sheet = sheets[0]
-
-  const students: ExcelCreateStudentsBatch[] = xlsx.utils.sheet_to_json(workbook.Sheets[sheet])
-
-  return students.map(item => ({
-    cpf: formatCPF(String(item['CPF'])),
-    username: item['NOME COMPLETO'],
-    email: item['E-MAIL'],
-    civilId: String(item['RG CIVIL']),
-    birthday: item['DATA DE NASCIMENTO'],
-    courseName: item['CURSO'],
-    poleName: item['POLO']
-  })) 
-}
-
-export function updateStudentsBatchExcelToJSON(fileUrl: string) {
-  const workbook = xlsx.readFile(resolve(cwd(), fileUrl))
-
-  const sheets = workbook.SheetNames
-  const sheet = sheets[0]
-
-  const students: ExcelUpdateStudentsBatch[] = xlsx.utils.sheet_to_json(workbook.Sheets[sheet])
-
-  return students.map(item => ({
-    cpf: String(item['CPF']),
-    username: item['NOME COMPLETO'],
-    email: item['E-MAIL'],
-    civilId: item['RG CIVIL'] ? String(item['RG CIVIL']) : undefined,
-    birthday: item['DATA DE NASCIMENTO'],
-    courseName: item['CURSO'],
-    poleName: item['POLO']
-  })) 
-}
-
-export function assessmentsBatchExcelToJSON(fileUrl: string) {
-  const workbook = xlsx.readFile(resolve(cwd(), fileUrl))
-
-  const sheets = workbook.SheetNames
-  const sheet = sheets[0]
-
-  const assessments: ExcelAssessmentsBatch[] = xlsx.utils.sheet_to_json(workbook.Sheets[sheet])
-
-  return assessments.map(item => ({
-    cpf: String(item['CPF']),
-    disciplineName: item['DISCIPLINA'],
-    vf: item['VF'],
-    avi: item['AVI'],
-    avii: item['AVII'],
-    vfe: item['VFE'],
-  })) 
-}
-
-export function behaviorsBatchExcelToJSON(fileUrl: string) {
-  const workbook = xlsx.readFile(resolve(cwd(), fileUrl))
-
-  const sheets = workbook.SheetNames
-  const sheet = sheets[0]
-
-  const behaviors: ExcelBehaviorsBatch[] = xlsx.utils.sheet_to_json(workbook.Sheets[sheet])
-
-  return behaviors.map(item => ({
-    cpf: String(item['CPF']),
-    january: item['JANEIRO'],
-    february: item['FEVEREIRO'],
-    march: item['MARÇO'],
-    april: item['ABRIL'],
-    may: item['MAIO'],
-    jun: item['JUNHO'],
-    july: item['JULHO'],
-    august: item['AGOSTO'],
-    september: item['SETEMBRO'],
-    october: item['OUTUBRO'],
-    november: item['NOVEMBRO'],
-    december: item['DEZEMBRO'],
-  })) 
-}
-=======
   const workbook = readFile(join(cwd(), fileUrl))
   const worksheet = workbook.Sheets[workbook.SheetNames[0]]
 
@@ -219,4 +121,3 @@ export function behaviorsBatchExcelToJSON(fileUrl: string) {
   }))
 }
 
->>>>>>> 475cc375b3f2ea5b2df4391ba52c8f454b9494a4
