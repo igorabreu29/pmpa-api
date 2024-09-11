a16e54d7dc94d7f699228189254a6b7ab52d4cb3
igorabreu29 <abreusense@gmail.com>
2024-09-11 19:05:34 -0300

    feat: merge files

--- src/domain/boletim/app/utils/classification/ranks-students-by-average-pole.ts
+++ src/domain/boletim/app/utils/classification/ranks-students-by-average-pole.ts
@@ -1,5 +1,6 @@
 import type { UniqueEntityId } from "@/core/entities/unique-entity-id.ts"
 import type { GetGeralStudentAverageStatusResponse } from "../get-geral-student-average-status.ts";
+import type { GenerateBehaviorStatus } from "../get-behavior-average-status.ts";
 
 export interface StudentWithAssessmentAverage {
     assessmentsAverage: {
@@ -15,18 +16,32 @@ export interface StudentWithAssessmentAverage {
     };
 }
 
-export interface AssessmentClassification {
-  assessmentAverageByPole: {
+export interface StudentWithBehaviorAverage {
+  behaviorAverage: {
+    behaviorAverageStatus: GenerateBehaviorStatus
+    behaviorsCount: number
+  }
+  studentName: string
+  studentBirthday: Date
+  studentCivilID: string
+  studentPole: {
+    id: UniqueEntityId
+    name: string
+  }
+}
+
+export interface PoleAverageClassification {
+  poleAverage: {
     poleId: string
     name: string
     average: number
   }
 }
 
-export const ranksStudentsByAssessmentAverage = (assessmentsAverage: AssessmentClassification[]) => {
+export const ranksStudentsByAveragePole = (assessmentsAverage: PoleAverageClassification[]) => {
   return assessmentsAverage.sort((studentA, studentB) => {
-    const geralAverageStudentA = studentA.assessmentAverageByPole.average
-    const geralAverageStudentB = studentB.assessmentAverageByPole.average
+    const geralAverageStudentA = studentA.poleAverage.average
+    const geralAverageStudentB = studentB.poleAverage.average
 
     if (geralAverageStudentA !== geralAverageStudentB) {
       return Number(geralAverageStudentB) - Number(geralAverageStudentA)
