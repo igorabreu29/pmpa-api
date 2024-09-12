9658adf7c6d932c90607703d73f4217631ab85b6
igorabreu <abreusense@gmail.com>
2024-09-12 15:10:23 -0300

    refactor: change default value to WEB_PORT

--- src/infra/env/index.ts
+++ src/infra/env/index.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'production', 'test']).default('dev'),
-  WEB_PORT: z.coerce.number().default(3000),
+  WEB_PORT: z.coerce.number().default(5173),
   API_PORT: z.coerce.number().default(3333),
   DATABASE_URL: z.string(),
   JWT_SECRET: z.string(),
