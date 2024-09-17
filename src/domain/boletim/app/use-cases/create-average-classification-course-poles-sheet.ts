8f2f81903fdd2dcfa8f67d77395fd7b2b8b69720
igorabreu29 <abreusense@gmail.com>
2024-09-17 10:17:20 -0300

    feat: add type response

--- src/domain/boletim/app/use-cases/create-average-classification-course-poles-sheet.ts
+++ src/domain/boletim/app/use-cases/create-average-classification-course-poles-sheet.ts
@@ -1,12 +1,17 @@
-import { left, right } from "@/core/either.ts";
+import { Either, left, right } from "@/core/either.ts";
 import type { CoursesRepository } from "../repositories/courses-repository.ts";
 import type { Sheeter } from "../sheet/sheeter.ts";
 import type { GetAverageClassificationCoursePolesUseCase } from "./get-average-classification-course-poles.ts";
 import { ResourceNotFoundError } from "@/core/errors/use-case/resource-not-found-error.ts";
+import { InvalidCourseFormulaError } from "./errors/invalid-course-formula-error.ts";
 
 interface CreateAverageClassificationCoursePolesSheetUseCaseRequest {
   courseId: string
 }
+
+type CreateAverageClassificationCoursePolesSheetUseCaseResponse = Either<ResourceNotFoundError | InvalidCourseFormulaError, {
+  filename: string
+}>
 
 export class CreateAverageClassificationCoursePolesSheetUseCase {
   constructor (
@@ -15,7 +20,7 @@ export class CreateAverageClassificationCoursePolesSheetUseCase {
     private sheeter: Sheeter
   ) {}
   
-  async execute({ courseId }: CreateAverageClassificationCoursePolesSheetUseCaseRequest) {
+  async execute({ courseId }: CreateAverageClassificationCoursePolesSheetUseCaseRequest): Promise<CreateAverageClassificationCoursePolesSheetUseCaseResponse> {
     const course = await this.coursesRepository.findById(courseId)
     if (!course) return left(new ResourceNotFoundError('Course not found.'))
 
