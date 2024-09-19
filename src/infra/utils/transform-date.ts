401637ff836dc2bb8d57d7ef79590782c3d3a461
igorabreu29 <abreusense@gmail.com>
2024-09-19 20:34:19 -0300

    refactor: change tranformDate

--- src/infra/utils/transform-date.ts
+++ src/infra/utils/transform-date.ts
@@ -1,5 +1,5 @@
 export function transformDate(dateToTransform: string) {
-  const [day, month, year] = dateToTransform.split('/')
+  const [year, month, day] = dateToTransform.split('/')
 
   const date = new Date()
   date.setFullYear(Number(year), Number(month), Number(day))
