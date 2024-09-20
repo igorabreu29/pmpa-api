22ed3d0fa4d131e9bd22a9be1001cfc569cd2ec2
igorabreu29 <abreusense@gmail.com>
2024-09-20 10:10:02 -0300

    feat: add startAt and endsAt

--- src/domain/boletim/app/use-cases/update-course.ts
+++ src/domain/boletim/app/use-cases/update-course.ts
@@ -4,16 +4,21 @@ import { ResourceNotFoundError } from "@/core/errors/use-case/resource-not-found
 import { Name } from "../../enterprise/entities/value-objects/name.ts";
 import type { InvalidNameError } from "@/core/errors/domain/invalid-name.ts";
 import type { Formula } from "../../enterprise/entities/course.ts";
+import { InvalidDateError } from "@/core/errors/domain/invalid-date.ts";
+import { EndsAt } from "../../enterprise/entities/value-objects/ends-at.ts";
 
 interface UpdateCourseUseCaseRequest {
   id: string
   name?: string
   formula?: Formula
+  startAt?: Date
+  endsAt?: Date
 }
 
 type UpdateCourseUseCaseResponse = Either<
   | ResourceNotFoundError
-  | InvalidNameError, null>
+  | InvalidNameError
+  | InvalidDateError, null>
 
 export class UpdateCourseUseCase {
   constructor(
@@ -24,6 +29,8 @@ export class UpdateCourseUseCase {
     id,
     name,
     formula,
+    startAt,
+    endsAt
   }: UpdateCourseUseCaseRequest): Promise<UpdateCourseUseCaseResponse> {
     const course = await this.coursesRepository.findById(id)
     if (!course) return left(new ResourceNotFoundError('Course not found.'))
@@ -31,7 +38,12 @@ export class UpdateCourseUseCase {
     const nameOrError = Name.create(name ?? course.name.value)
     if (nameOrError.isLeft()) return left(nameOrError.value)
 
+    const endsAtOrError = EndsAt.create(endsAt ?? course.endsAt.value)
+    if (endsAtOrError.isLeft()) return left(endsAtOrError.value)
+
     course.name = nameOrError.value
+    course.startAt = startAt ?? course.startAt
+    course.endsAt = endsAtOrError.value
     course.formula = formula ?? course.formula
     course.isPeriod = !['CAS', 'CHO', 'CFP', 'CGS'].includes(course.formula)
 
--- src/infra/http/controllers/update-course.ts
+++ src/infra/http/controllers/update-course.ts
@@ -4,12 +4,13 @@ import { verifyJWT } from "../middlewares/verify-jwt.ts";
 import { verifyUserRole } from "../middlewares/verify-user-role.ts";
 import { z } from "zod";
 import { makeUpdateCourseUseCase } from "@/infra/factories/make-update-course-use-case.ts";
-import { ResourceAlreadyExistError } from "@/core/errors/use-case/resource-already-exist-error.ts";
 import { Conflict } from "../errors/conflict-error.ts";
 import { InvalidNameError } from "@/core/errors/domain/invalid-name.ts";
 import { ClientError } from "../errors/client-error.ts";
 import { ResourceNotFoundError } from "@/core/errors/use-case/resource-not-found-error.ts";
 import { NotFound } from "../errors/not-found.ts";
+import { transformDate } from "@/infra/utils/transform-date.ts";
+import { InvalidDateError } from "@/core/errors/domain/invalid-date.ts";
 
 export async function updateCourse(
   app: FastifyInstance
@@ -26,18 +27,22 @@ export async function updateCourse(
         body: z.object({
           formula: z.enum(['CGS', 'CFP', 'CAS', 'CFO', 'CHO']),
           name: z.string().optional(),
+          startAt: z.string().transform(transformDate).optional(),
+          endsAt: z.string().transform(transformDate).optional(),
         })
       }
     },
       async (req, res) => {
         const { id } = req.params
-        const { formula, name } = req.body
+        const { formula, name, startAt, endsAt } = req.body
 
         const useCase = makeUpdateCourseUseCase()
         const result = await useCase.execute({
           id,
           formula,
           name,
+          startAt,
+          endsAt
         })
 
         if (result.isLeft()) {
@@ -47,6 +52,8 @@ export async function updateCourse(
             case ResourceNotFoundError: 
               throw new NotFound(error.message)
             case InvalidNameError:  
+              throw new Conflict(error.message)
+            case InvalidDateError:  
               throw new Conflict(error.message)
             default: 
               throw new ClientError()
