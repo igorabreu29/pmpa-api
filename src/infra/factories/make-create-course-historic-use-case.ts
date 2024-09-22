d7edd3ad0cede6b90ba0f3a1ad9b7a54bc5abac3
igorabreu29 <abreusense@gmail.com>
2024-09-22 15:02:25 -0300

    feat: add course disciplines repository

--- src/infra/factories/make-create-course-historic-use-case.ts
+++ src/infra/factories/make-create-course-historic-use-case.ts
@@ -1,12 +1,15 @@
+import { PrismaCourseDisciplinesRepository } from "../database/repositories/prisma-course-disciplines-repository.ts";
 import { PrismaCourseHistoricsRepository } from "../database/repositories/prisma-course-historics-repository.ts";
 import { PrismaCoursesRepository } from "../database/repositories/prisma-courses-repository.ts";
 import { CreateCourseHistoricUseCase } from "@/domain/boletim/app/use-cases/create-course-historic.ts";
 
 export function makeCreateCourseHistoricUseCase() {
   const coursesRepository = new PrismaCoursesRepository()
+  const courseDisciplinesRepository = new PrismaCourseDisciplinesRepository()
   const courseHistoricRepository = new PrismaCourseHistoricsRepository()
   return new CreateCourseHistoricUseCase(
     coursesRepository,
+    courseDisciplinesRepository,
     courseHistoricRepository
   )
 }
