8c46d9ff0a8513c5e4432b48d4bf5e269eee576a
igorabreu29 <abreusense@gmail.com>
2024-09-25 12:30:49 -0300

    feat: add new envs

--- src/infra/env/index.ts
+++ src/infra/env/index.ts
@@ -3,9 +3,16 @@ import { z } from "zod";
 export const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'production', 'test']).default('dev'),
   WEB_PORT: z.coerce.number().default(5173),
+  WEB_URL: z.string().url(),
   API_PORT: z.coerce.number().default(3333),
   DATABASE_URL: z.string(),
   JWT_SECRET: z.string(),
+  MAIL_HOST: z.string(),
+  MAIL_PORT: z.coerce.number(),
+  MAIL_USER: z.string(),
+  MAIL_PASS: z.string(),
+  REDIS_HOST: z.string(),
+  REDIS_PORT: z.coerce.number(),
 })
 
 export const env = envSchema.parse(process.env)
