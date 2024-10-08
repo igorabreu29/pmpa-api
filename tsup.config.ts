faf7819e77897f92af85e354d2b0d7dbd10737e2
igorabreu29 <abreusense@gmail.com>
2024-10-08 08:02:16 -0300

    chore: update tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,8 +4,11 @@ export default defineConfig({
   entry: ['src/**/*.ts', '!src/**/*.spec.ts', '!src/**/*.e2e-spec.ts'],
   outDir: 'dist',
   splitting: false,
+  dts: false,
+  format: ['cjs', 'esm'],
   sourcemap: true,
   clean: true,
+  target: 'es6',
   loader: {
     '.html': 'file',
   },
