ea79a781897dd333ce4ee17d9db6bf96d25e1382
igorabreu29 <abreusense@gmail.com>
2024-10-08 09:21:38 -0300

    feat: add host

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -2,6 +2,7 @@ import { app } from "../app.ts";
 import { env } from "../env/index.ts";
 
 app.listen({
-  port: env.API_PORT
+  host: '0.0.0.0',
+  port: env.API_PORT 
 })  
   .then(() => console.log(`Server running on port ${env.API_PORT}`))
