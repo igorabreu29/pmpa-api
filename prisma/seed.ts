ecd8ecc3dd15066580a4b5967a21328497f6665c
igorabreu29 <abreusense@gmail.com>
2024-10-09 12:57:04 -0300

    feat: update seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -31,54 +31,98 @@ async function seed() {
 
   const pole = await prisma.pole.create({
     data: {
-      name: 'CFAP',
+      name: 'APM',
     }
   })
 
-  const course = await prisma.course.create({
+  const discipline = await prisma.discipline.create({
     data: {
-      name: 'CAS',
+      name: 'discipline-1'
+    }
+  })
+
+  const discipline2 = await prisma.discipline.create({
+    data: {
+      name: 'discipline-2'
+    }
+  })
+
+  const discipline3 = await prisma.discipline.create({
+    data: {
+      name: 'discipline-3'
+    }
+  })
+
+  await prisma.course.create({
+    data: {
+      name: 'CFO 2022',
       endsAt: new Date('2050-01-02'),
-      formula: 'CAS',
+      formula: 'CFO',
       imageUrl: '',
+      isPeriod: true,
 
       courseOnPoles: {
         create: {
           poleId: pole.id
         }
+      },
+
+      coursesOnDisciplines: {
+        createMany: {
+          data: [
+            {
+              disciplineId: discipline.id,
+              expected: 'VF',
+              hours: 30,
+              module: 1
+            },
+            {
+              disciplineId: discipline2.id,
+              expected: 'VF',
+              hours: 30,
+              module: 1
+            },
+            {
+              disciplineId: discipline3.id,
+              expected: 'VF',
+              hours: 30,
+              module: 1
+            },
+          ]
+        }
       }
     },
   })
 
-  const studentsToInsert: Prisma.UserUncheckedCreateInput[] = []
+  // const studentsToInsert: Prisma.UserUncheckedCreateInput[] = []
 
-  for (let i = 11; i <= 91; i++) {
-    studentsToInsert.push({
-      cpf: `123456789${i}`,
-      email: faker.internet.email(),
-      username: faker.person.fullName(),
-      civilId: '00000',
-      password: '$2a$08$IIGNU2kgBV5B9EEQmmm.g.cZy8n1WpEl9xJ.39Qd9f4lNiFXt1Xji',
-      birthday: faker.date.birthdate(),
+  // for (let i = 11; i <= 91; i++) {
+  //   studentsToInsert.push({
+  //     cpf: `123456789${i}`,
+  //     email: faker.internet.email(),
+  //     username: faker.person.fullName(),
+  //     civilId: '00000',
+  //     password: '$2a$08$IIGNU2kgBV5B9EEQmmm.g.cZy8n1WpEl9xJ.39Qd9f4lNiFXt1Xji',
+  //     birthday: faker.date.birthdate(),
 
-      usersOnCourses: {
-        create: {
-          courseId: course.id,
-          usersOnPoles: {
-            create: {
-              poleId: pole.id
-            }
-          }
-        }
-      }
-    })
-  }
+  //     usersOnCourses: {
+  //       create: {
+  //         courseId: course.id,
+  //         usersOnPoles: {
+  //           create: {
+  //             poleId: pole.id
+  //           }
+  //         }
+  //       }
+  //     }
+  //   })
+  // }
 
-  await Promise.all(studentsToInsert.map(async student => {
-    await prisma.user.create({
-      data: student
-    })
-  }))
+  // await Promise.all(studentsToInsert.map(async student => {
+  //   await prisma.user.create({
+  //     data: student
+  //   })
+  // }))
 }
 
 seed()  
