1a6cb153d7c470fe9d7c7bb48721d3b2aa4ce00a
igorabreu29 <abreusense@gmail.com>
2024-10-10 17:48:43 -0300

    feat: reason

--- src/infra/database/mappers/prisma-manager-course-details.ts
+++ src/infra/database/mappers/prisma-manager-course-details.ts
@@ -1,10 +1,12 @@
 import { UniqueEntityId } from '@/core/entities/unique-entity-id.ts';
 import { ManagerCourseDetails } from '@/domain/boletim/enterprise/entities/value-objects/manager-course-details.ts';
-import { Course, Pole, User } from '@prisma/client';
+import { Course, Pole, Prisma, User, UserOnCourse } from '@prisma/client';
 
 type PrismaManagerWithCourseAndPole = User & {
   course: Course
   pole: Pole
+  userOnCourse: UserOnCourse
+  profile?: Prisma.ProfileUncheckedCreateInput
 }
 
 export class PrismaManagerCourseDetailsMapper {
@@ -13,8 +15,19 @@ export class PrismaManagerCourseDetailsMapper {
       managerId: new UniqueEntityId(manager.id),
       email: manager.email,
       cpf: manager.cpf,
+      isActive: manager.userOnCourse.isActive,
+      reason: manager.userOnCourse.reason ?? undefined,
+      civilId: manager.civilId,
+      militaryId: manager.profile?.militaryId ?? undefined,
+      state: manager.profile?.state ?? undefined,
+      county: manager.profile?.county ?? undefined,
+      parent: {
+        fatherName: manager.profile?.fatherName ?? undefined,
+        motherName: manager.profile?.motherName ?? undefined
+      },
       assignedAt: manager.createdAt,
       username: manager.username,
+      birthday: manager.birthday as Date,
       course: manager.course.name,
       courseId: new UniqueEntityId(manager.course.id),
       pole: manager.pole.name,
