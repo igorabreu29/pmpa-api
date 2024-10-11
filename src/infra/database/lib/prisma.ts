59bf51b0d3c20ea76846353d58de79c2d4e0adfb
igorabreu29 <abreusense@gmail.com>
2024-10-11 10:45:48 -0300

    feat: prisma

--- src/infra/database/lib/prisma.ts
+++ src/infra/database/lib/prisma.ts
@@ -1,20 +1,5 @@
-import { env } from "@/infra/env/index.ts";
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient 
-
-if (env.NODE_ENV === 'production') {
-  prisma = new PrismaClient({
-    log: ['error']
-  })
-}
-
-if (env.NODE_ENV !== 'production') {
-  prisma = new PrismaClient({
-    log: ['error', 'query']
-  })
-}
-
-export {
-  prisma
-}
+export const prisma = new PrismaClient({
+  log: ['error']
+})
