a16ad49b10cc1cf2a33029bf661f9fc836c85f2d
igorabreu29 <abreusense@gmail.com>
2024-10-21 21:09:18 -0300

    feat: classifications mapper

---
+++ prisma/migrations/20241022000445_add_behaviors_as_optional/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "Classification" ALTER COLUMN "behaviorAverageModule1" DROP NOT NULL,
+ALTER COLUMN "behaviorAverageModule2" DROP NOT NULL,
+ALTER COLUMN "behaviorAverageModule3" DROP NOT NULL,
+ALTER COLUMN "behaviorStatusModule1" DROP NOT NULL,
+ALTER COLUMN "behaviorStatusModule2" DROP NOT NULL,
+ALTER COLUMN "behaviorStatusModule3" DROP NOT NULL;
